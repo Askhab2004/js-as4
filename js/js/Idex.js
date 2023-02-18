e360082e534ce725fee4cc352f3ee42b58a7bcbb
Askhab2004 <123107421+Askhab2004@users.noreply.github.com>
2023-02-18 16:00:01 +0300

    js as4
    js

---
+++ js/js/Idex.js
@@ -0,0 +1,43 @@
+//№1
+let name = 'Askhab'
+
+//№3
+const age = '18'
+
+//№4
+if (age < 15){
+    console.log(`Доступ запрещен`)
+}else{
+    console.log(`Привет ${name}`)
+}
+
+//№5
+function viewMoney(){
+    console.log('Вот вам 10$')
+}
+viewMoney()
+
+//№6
+function getMoney(a){
+    return a;
+}
+getMoney('Вот вам 10$');
+
+//#7
+function cash(s){
+    console.log(`Вот вам ${s}$`)
+}
+cash(399)
+
+//8
+function func(q) {
+    console.log(Math.pow(q,2));
+}
+func(6);
+
+//9
+function rar (r, e, w){
+    console.log(Math.max(r, e, w))
+}
+rar(888, 400, 777)
+
